512ad84eb5b9d768c92dcede4214df33fe14556c
Shishir Halaharvi <shihala1@in.ibm.com>
2019-11-26 15:28:26 +0530

    Remove repeated calls to getPodMetrics
    Signed-off-by: Shishir Halaharvi <shihala1@in.ibm.com>

--- src/main/java/com/kruize/metrics/PodMetrics.java
+++ src/main/java/com/kruize/metrics/PodMetrics.java
@@ -18,22 +18,103 @@ package com.kruize.metrics;
 
 import com.kruize.recommendations.instance.PodRecommendations;
 import com.kruize.recommendations.instance.Recommendations;
+import io.kubernetes.client.custom.Quantity;
+import io.kubernetes.client.models.V1Pod;
+import io.kubernetes.client.models.V1ResourceRequirements;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PodMetrics extends AbstractMetrics
 {
-    private String podTemplateHash;
 
     private Recommendations y2dRecommendations = new PodRecommendations();
     private Recommendations currentRecommendations = new PodRecommendations();
 
-    public String getPodTemplateHash()
+    private PodMetrics() { }
+
+    public PodMetrics(V1Pod pod)
     {
-        return podTemplateHash;
+        PodMetrics podMetrics = new PodMetrics();
+        podMetrics.setName(pod.getMetadata().getName());
+        podMetrics.setNamespace(pod.getMetadata().getNamespace());
+        podMetrics.setStatus(pod.getStatus().getPhase());
+
+        String applicationName = parseApplicationName(pod.getMetadata().getName(),
+                getPodTemplateHash(pod));
+
+        podMetrics.setApplicationName(applicationName);
+
+        setOriginalRequestsAndLimits(pod, podMetrics);
     }
 
-    public void setPodTemplateHash(String podTemplateHash)
+    private static void setOriginalRequestsAndLimits(V1Pod pod, PodMetrics podMetrics)
     {
-        this.podTemplateHash = podTemplateHash;
+        V1ResourceRequirements resources = pod.getSpec().getContainers().get(0).getResources();
+
+        Map podRequests = resources.getRequests();
+        Map podLimits = resources.getLimits();
+
+        if (podRequests != null) {
+            if (podRequests.containsKey("memory")) {
+                Quantity memoryRequests = (Quantity) podRequests.get("memory");
+                System.out.println(memoryRequests.getNumber().doubleValue());
+                podMetrics.setOriginalMemoryRequests(memoryRequests.getNumber().doubleValue());
+            }
+
+            if (podRequests.containsKey("cpu")) {
+                Quantity cpuRequests = (Quantity) podRequests.get("cpu");
+                podMetrics.setOriginalCpuRequests(cpuRequests.getNumber().doubleValue());
+            }
+        }
+
+        if (podLimits != null) {
+            if (podLimits.containsKey("memory")) {
+                Quantity memoryLimit = (Quantity) podLimits.get("memory");
+                podMetrics.setOriginalMemoryLimit(memoryLimit.getNumber().doubleValue());
+            }
+
+            if (podLimits.containsKey("cpu")) {
+                Quantity cpuLimit = (Quantity) podLimits.get("cpu");
+                podMetrics.setOriginalCpuLimit(cpuLimit.getNumber().doubleValue());
+            }
+        }
+    }
+
+    public static String parseApplicationName(String podName, String hash)
+    {
+        if (hash != null) {
+            return podName.substring(0, podName.indexOf(hash) - 1);
+        } else {
+            return parseApplicationNameFromInstanceName(podName);
+        }
+    }
+
+    private static String parseApplicationNameFromInstanceName(String podName)
+    {
+        Pattern pattern = Pattern.compile("-[a-zA-Z]*?\\d+");
+        Matcher matcher = pattern.matcher(podName);
+
+        if (matcher.find()) {
+            int index = matcher.start();
+            return podName.substring(0, index);
+        }
+        return podName;
+    }
+
+    public static String getPodTemplateHash(V1Pod pod)
+    {
+        try
+        {
+            String podHashLabel = "pod-template-hash";
+
+            return pod.getMetadata()
+                    .getLabels().get(podHashLabel);
+        }
+        catch (NullPointerException e) {
+            return null;
+        }
     }
 
     public double getCpuRequests()
