4aa48c651e1b280497bf14cfcf9217b8d566e810
Shishir Halaharvi <shihala1@in.ibm.com>
2020-04-20 09:05:00 +0530

    Add java runtimes recommendation abstraction and implement OpenJ9JavaRecommendations
    Signed-off-by: Shishir Halaharvi <shihala1@in.ibm.com>

---
+++ src/main/java/com/kruize/recommendations/runtimes/java/openj9/OpenJ9JavaRecommendations.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2019, 2020 IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.kruize.recommendations.runtimes.java.openj9;
+
+import com.kruize.recommendations.runtimes.java.JavaRecommendations;
+
+public class OpenJ9JavaRecommendations implements JavaRecommendations
+{
+    /* TODO Also add nursery recommendations and GC */
+    private String gcPolicy = "gencon";
+    private double heapRecommendation;
+    private double nonHeapRecommendation;
+
+    @Override
+    public double getHeapRecommendation()
+    {
+        return heapRecommendation;
+    }
+
+    @Override
+    public void setHeapRecommendation(double heapRecommendation)
+    {
+        this.heapRecommendation = heapRecommendation;
+    }
+
+    @Override
+    public double getNonHeapRecommendation()
+    {
+        return nonHeapRecommendation;
+    }
+
+    @Override
+    public void setNonHeapRecommendation(double nonHeapRecommendation)
+    {
+        this.nonHeapRecommendation = nonHeapRecommendation;
+    }
+
+    @Override
+    public String getGcPolicy()
+    {
+        return gcPolicy;
+    }
+
+    @Override
+    public void setGcPolicy(String gcPolicy)
+    {
+        this.gcPolicy = gcPolicy;
+    }
+}
